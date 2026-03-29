d55b0a9dd1a3215d4c8db6ba6a0d60901fc975c6
unknown <21je0293@iitism.ac.in>
2026-03-29 21:15:23 +0530

    maths q1

---
+++ striver-cp-sheet/maths/Ichihime_and_Triangle.cpp
@@ -0,0 +1,21 @@
+//https://codeforces.com/problemset/problem/1337/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+//a+b>c b+c>a c+a>b
+//for a triangle
+
+int32_t main(){
+
+    int t;cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+    return 0;
+}
